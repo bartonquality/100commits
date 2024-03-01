e7223b8ad6a13722baf6430ab649de4c4790846e
Barton <bartonquality164@gmail.com>
2024-03-01 20:44:26 +0100

    Initial Commit

---
+++ src/SchoolDiary/SchoolDiary/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SchoolDiary.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
